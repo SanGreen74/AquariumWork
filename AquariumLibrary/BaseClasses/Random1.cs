f69c9e65a58cde4b70904f24f8bcdf19d6f99a1c
vanek98 <ivan-potapov1998@mail.ru>
2017-12-13 12:26:38 +0500

    add Piranha

---
+++ AquariumLibrary/BaseClasses/Random1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquariumLibrary.BaseClasses
+{
+    public static class Random1
+    {
+        public static Random rnd = new Random();
+    }
+}
