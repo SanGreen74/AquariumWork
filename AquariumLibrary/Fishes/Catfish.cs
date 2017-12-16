781a4bc16ac07e3487b26e6a5920e97149cc41e5
romadrimm <34436101+romadrimm@users.noreply.github.com>
2017-12-16 09:22:53 +0300

    add Function Catfish

--- AquariumLibrary/Fishes/Catfish.cs
+++ AquariumLibrary/Fishes/Catfish.cs
@@ -2,13 +2,16 @@
 using System;
 using AquariumLibrary.Interfaces;
 using System.Drawing;
+using AquariumLibrary.BaseClasses;
 
 namespace AquariumLibrary.Fishes
 {
     public class Catfish : AFish
     {
+        
         protected override PointF GetNextPoint()
         {
+            
             var random = new Random();
             while(true)
             {
@@ -31,12 +34,45 @@ namespace AquariumLibrary.Fishes
             if (CheckCorrectLocation(location) == false)
                 throw new ArgumentException("Точка не принадлежит области обитания");
             Speed = 2;
+            PushState(Walking);
+        }
+        private void Walking()
+        {
+            if (nextPointSleep == null)
+                nextPointSleep = RandomPoint();
+            var vector = new VectorF(Location, (PointF)nextPointSleep);
+            if(vector.GetLength() < GetStepLength())
+            {
+                nextPointSleep = null;
+                PushState(Sleep);
+                return;
+            }
+            var point = GetNextPoint();
+            MoveTo(point);
+        }
+        private void Sleep()
+        {
+            //todo
+            MoveTo(Location);
         }
         private double CatfishBroad;
         private bool CheckCorrectLocation(PointF location)
         {
             return location.Y >= CatfishBroad;
         }
-
+        private PointF RandomPoint()
+        {
+            var rndX = Random1.rnd.Next((int)CatfishBroad, Aquarium.Size.Height);
+            var rndY = Random1.rnd.Next(0, Aquarium.Size.Width);
+            var point1 = new PointF(rndX, rndY);
+            return point1;
+        }
+        private PointF? nextPointSleep;   
+       public double GetStepLength()
+        {
+            var dx = (float)Speed * Direction.X;
+            var dy = (float)Speed * Direction.Y;
+            return Math.Sqrt(dx*dx + dy*dy);
+        }   
     }
 }
